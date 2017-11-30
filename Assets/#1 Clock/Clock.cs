f9cdd9ead5adc49638972a9d84e6aa57407ed87c
Gaurav Chande <gmail@gauravchande.com>
2017-11-30 16:56:00 +0530

    Custom shader with Graph

---
+++ Assets/#1 Clock/Clock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+	private const float degressPerHour = 30f;
+	private const float degressPerMinute = 6f;
+	private const float degressPerSecond = 6f;
+
+	public Transform hoursTransform, minutesTransform, secondsTransform;
+
+	void Update() {
+		DateTime time = DateTime.Now; // To show Second as discrete
+		TimeSpan timeSpan = time.TimeOfDay; // To show Hour and Minute as continuous
+
+		hoursTransform.localRotation = Quaternion.Euler(0f, (float) timeSpan.TotalHours * degressPerHour, 0f);
+		minutesTransform.localRotation = Quaternion.Euler(0f, (float) timeSpan.TotalMinutes * degressPerMinute, 0f);
+		secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degressPerSecond, 0f);
+	}
+}
