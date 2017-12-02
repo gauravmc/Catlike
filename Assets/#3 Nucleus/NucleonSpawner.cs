2ffec6d49ae14f45171733061846c05e43e0c85f
Gaurav Chande <gmail@gauravchande.com>
2017-12-03 01:17:00 +0530

    Cool Nucleus spawner!

---
+++ Assets/#3 Nucleus/NucleonSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+	public float timeBetweenSpawns;
+	public float spawnDistance;
+	public Nucleon[] nucleonPrefabs;
+
+	private float timeSinceLastSpawn;
+
+	private void Awake() {
+		SpawnNucleon();
+		timeSinceLastSpawn = 0;
+	}
+
+	private void FixedUpdate() {
+		if (timeSinceLastSpawn >= timeBetweenSpawns) {
+			SpawnNucleon();
+			timeSinceLastSpawn = 0;
+		} else {
+			timeSinceLastSpawn += Time.deltaTime;
+		}
+	}
+
+	private void SpawnNucleon() {
+		Nucleon randomNucleon = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+		Nucleon spawn = Instantiate(randomNucleon);
+		spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+	}
+}
