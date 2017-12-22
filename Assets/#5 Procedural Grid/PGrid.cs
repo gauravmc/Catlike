78c8901cf594b8c261c53502a0a2f7449e59d1c4
Gaurav Chande <gmail@gauravchande.com>
2017-12-22 14:36:00 +0530

    Procedural Grid and early part of Rounded Cube

---
+++ Assets/#5 Procedural Grid/PGrid.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class PGrid : MonoBehaviour {
+	public int xSize, ySize;
+
+	private Vector3[] vertices;
+	private Mesh mesh;
+
+	private void Awake() {
+		Generate();
+	}
+
+	private void Generate() {
+		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+		mesh.name = "Procedural Grid";
+
+		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+		Vector2[] uv = new Vector2[vertices.Length];
+		// Vector4[] tangents = new Vector4[vertices.Length];
+		// Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+		for (int i = 0, y = 0; y <= ySize; y++) {
+			for (int x = 0; x <= xSize; x++, i++) {
+				vertices[i] = new Vector3(x, y);
+				uv[i] = new Vector2((float) x / xSize, (float) y / ySize);
+				// tangents[i] = tangent;
+			}
+		}
+		mesh.vertices = vertices;
+		mesh.uv = uv;
+		// mesh.tangents = tangents;
+
+		int[] triangles = new int[xSize * ySize * 6];
+		for (int vi = 0, ti = 0, y = 0; y < ySize; y++, vi++) {
+			for (int x = 0; x < xSize; ti += 6, x++, vi++) {
+				triangles[ti] = vi;
+				triangles[ti + 2] = triangles[ti + 3] = vi + 1;
+				triangles[ti + 1] = triangles[ti + 4] = vi + xSize + 1;
+				triangles[ti + 5] = vi + xSize + 2;
+			}
+		}
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals();
+	}
+//
+//	private void OnDrawGizmos() {
+//		if (vertices == null) {
+//			return;
+//		}
+//
+//		Gizmos.color = Color.black;
+//		foreach(Vector3 vertex in vertices) {
+//			Gizmos.DrawSphere(vertex, 0.1f);
+//		}
+//	}
+}
