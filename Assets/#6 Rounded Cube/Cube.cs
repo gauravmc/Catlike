12b1e90a0fe9175b050633f1d31601df83899ec8
Gaurav Chande <gmail@gauravchande.com>
2017-12-22 16:35:00 +0530

    Create vertices

--- Assets/#6 Rounded Cube/Cube.cs
+++ Assets/#6 Rounded Cube/Cube.cs
@@ -9,43 +9,68 @@ public class Cube : MonoBehaviour {
 	private Vector3[] vertices;
 
 	private void Awake() {
-		StartCoroutine(Generate());
+		Generate();
 	}
 
-	private IEnumerator Generate() {
+	private void Generate() {
 		WaitForSeconds wait = new WaitForSeconds(0.05f);
 		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
 		mesh.name = "Procedural Cube";
+		vertices = new Vector3[CalculateNumberOfVertices()];
+		CreateVertices();
+		CreateTriangles();
+	}
 
+	private int CalculateNumberOfVertices() {
 		int cornerVertices = 8;
 		int edgeVertices = (xSize + ySize + zSize - 3) * 4;
-		int faceVertices = (
-			(xSize - 1) * (ySize - 1) +
-			(xSize - 1) * (zSize - 1) +
-			(ySize - 1) * (zSize - 1)) * 2;
-		vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+		int faceVertices = ((xSize - 1) * (ySize - 1) +
+			(ySize - 1) * (zSize - 1) +
+			(zSize - 1) * (xSize - 1)) * 2;
 
+		return cornerVertices + edgeVertices + faceVertices;
+	}
+
+	private void CreateVertices() {
 		int v = 0;
+
 		for (int y = 0; y <= ySize; y++) {
 			for (int x = 0; x <= xSize; x++) {
 				vertices[v++] = new Vector3(x, y, 0);
-				yield return wait;
 			}
+
 			for (int z = 1; z <= zSize; z++) {
 				vertices[v++] = new Vector3(xSize, y, z);
-				yield return wait;
 			}
+
 			for (int x = xSize - 1; x >= 0; x--) {
 				vertices[v++] = new Vector3(x, y, zSize);
-				yield return wait;
 			}
+
 			for (int z = zSize - 1; z > 0; z--) {
 				vertices[v++] = new Vector3(0, y, z);
-				yield return wait;
 			}
 		}
 
-		yield return wait;
+		for (int x = 1; x < xSize; x++) {
+			for (int z = 1; z < zSize; z++) {
+				vertices[v++] = new Vector3(x, 0, z);
+			}
+		}
+
+		for (int x = 1; x < xSize; x++) {
+			for (int z = 1; z < zSize; z++) {
+				vertices[v++] = new Vector3(x, ySize, z);
+			}
+		}
+
+		mesh.vertices = vertices;
+	}
+
+	private void CreateTriangles() {
+		int quads = (xSize * ySize + ySize * zSize + zSize * xSize) * 2;
+		int[] triangles = new int[quads * 6];
+		mesh.triangles = triangles;
 	}
 
 	private void OnDrawGizmos() {
